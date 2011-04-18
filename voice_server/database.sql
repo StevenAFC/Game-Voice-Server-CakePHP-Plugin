ad98e2d96f4eb478a9b20bcd4e77df49a3a45a66
Chickenhen <nehnekcihc@gmail.com>
2011-04-18 21:27:22 +0100

    add license comments

--- voice_server/database.sql
+++ voice_server/database.sql
@@ -1,4 +1,27 @@
+/*
+ * Behavior for connecting to voice servers (Teamspeak, Teamspeak 3 and Ventrilo) and parsing the data.
+ *
+ * PHP versions 4 and 5
+ *
+ * Voice Server Plugin (http://www.boku.co.uk)
+ * Copyright 2011-2011, Boku LLP. (http://www.boku.co.uk)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2011-2011, Boku LLP. (http://www.boku.co.uk)
+ * @link          http://www.boku.co.uk Voice Server Plugin
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+ 
+ /*
+	Drop the table
+ */
 DROP TABLE IF EXISTS `game_voice_servers`;
+
+/*
+	Table definition
+ */
 CREATE TABLE  `game_voice_servers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `ip` varchar(45) NOT NULL,
@@ -8,4 +31,4 @@ CREATE TABLE  `game_voice_servers` (
   `cache` longtext NOT NULL,
   `cache_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
--- voice_server/voice_server_app_controller.php
+++ voice_server/voice_server_app_controller.php
@@ -1,4 +1,24 @@
 <?php
-	class VoiceServerAppController extends AppController {
-	}
+/**
+ * Behavior for connecting to voice servers (Teamspeak, Teamspeak 3 and Ventrilo) and parsing the data.
+ *
+ * PHP versions 4 and 5
+ *
+ * Voice Server Plugin (http://www.boku.co.uk)
+ * Copyright 2011-2011, Boku LLP. (http://www.boku.co.uk)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2011-2011, Boku LLP. (http://www.boku.co.uk)
+ * @link          http://www.boku.co.uk Voice Server Plugin
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+ 
+ /**
+ * Application controller for Voice Server.
+ *
+ */
+class VoiceServerAppController extends AppController {
+}
 ?>
